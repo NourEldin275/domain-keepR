dc7a1531d4d83f1250de3bb5061a5fbfe8c21139
Nour Eldin Ali Mohamed <noureldin.ali.m@gmail.com>
2016-12-10 12:08:34 +0200

    minor changes. disabled the custom JS temporarily that was created to handle ajax search requests.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,52 +1,52 @@
-$(document).ready(function () {
-
-    // attach keyup event
-
-    var timer;
-    var typingInterval = 4500;
-
-    $("#header-search").change(function (event) {
-
-        var search = $(this).val();
-
-        if ( search.length < 3 ){
-            return false;
-        }
-
-        clearTimeout(timer);
-        timer = setTimeout(searchApp(search), typingInterval);
-
-    });
-
-});
-
-function searchApp(search){
-
-    var url = 'http://localhost:8000/ajax/search';
-    $.get(
-        url,
-        {
-            'term': search
-        },
-        function (data) {
-            var result = '<div id="search-results">';
-
-            var domains = data.data.domains;
-            var clients = data.data.clients;
-
-            for ( var i = 0; i < domains.length; i++ ){
-                result += domains[i].domain + '<br>';
-            }
-
-            for ( var i = 0; i < clients.length; i++ ){
-                result += clients[i].name + '<br>';
-            }
-
-            result += '</div>';
-
-            $("#header-search").after(result);
-
-        }
-    );
-
-}
+// $(document).ready(function () {
+//
+//     // attach keyup event
+//
+//     var timer;
+//     var typingInterval = 4500;
+//
+//     $("#header-search").change(function (event) {
+//
+//         var search = $(this).val();
+//
+//         if ( search.length < 3 ){
+//             return false;
+//         }
+//
+//         clearTimeout(timer);
+//         timer = setTimeout(searchApp(search), typingInterval);
+//
+//     });
+//
+// });
+//
+// function searchApp(search){
+//
+//     var url = 'http://localhost:8000/ajax/search';
+//     $.get(
+//         url,
+//         {
+//             'term': search
+//         },
+//         function (data) {
+//             var result = '<div id="search-results">';
+//
+//             var domains = data.data.domains;
+//             var clients = data.data.clients;
+//
+//             for ( var i = 0; i < domains.length; i++ ){
+//                 result += domains[i].domain + '<br>';
+//             }
+//
+//             for ( var i = 0; i < clients.length; i++ ){
+//                 result += clients[i].name + '<br>';
+//             }
+//
+//             result += '</div>';
+//
+//             $("#header-search").after(result);
+//
+//         }
+//     );
+//
+// }
