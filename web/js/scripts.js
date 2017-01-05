1bb11c1d110e5c5650ebaf572f0fffc9391e8400
Nour Eldin Ali <noureldin.ali.m@gmail.com>
2017-01-05 11:41:34 +0200

    Changed the way Ajax is sent for header search. Made the script to wait for 1200 MS before sending the request after getting the current field value.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,3 +1,4 @@
+var searchTimer;
 jQuery(document).ready(function () {
 
     // Fire Ajax call when Add client button is clicked
@@ -20,33 +21,50 @@ function headerSearch(event) {
     var modalElement = '#search-results-modal';
     var modalBody = '#search-results-modal .modal-body';
 
+    clearTimeout(searchTimer);
+
+    // Check that key pressed was numbers/letters or backspace or period '.'
+    // return if it is not one of those keys
+    if ( !( (event.which > 47 && event.which < 91) || event.which == 190 )  ){
+        return;
+    }
+
     // If the event was keyup and fields value length was more than 4 or the event was click and field value was more than 4
     if ( event.type == 'keyup' && jQuery(this).val().length > 4 )
     {
 
-        jQuery.ajax({
-            type: 'GET',
-            url: Routing.generate('header_ajax_search'),
-            dataType: 'text',
-            data: {
-                term: jQuery(this).val()
-            },
-            success: function (response) {
-                jQuery(modalBody).html(response);
-                jQuery(modalElement).modal('show');
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
+        var searchField = jQuery(this);
 
-                var response = '<div class="alert alert-warning" role="alert">Something went wrong!</div>';
+        jQuery(searchField).off('keyup', headerSearch);
 
-                jQuery(modalBody).html(response);
-                jQuery(modalElement).modal('show');
+        searchTimer = setTimeout(function () {
+            if ( searchField.val().length > 4 ){
+                jQuery.ajax({
+                    type: 'GET',
+                    url: Routing.generate('header_ajax_search'),
+                    dataType: 'text',
+                    data: {
+                        term: searchField.val()
+                    },
+                    success: function (response) {
+                        jQuery(modalBody).html(response);
+                        jQuery(modalElement).modal('show');
+                        jQuery(searchField).on('keyup', headerSearch);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
 
-                setTimeout(function () {
-                   jQuery(modalElement).modal('hide');
-                }, 7000);
+                        var response = '<div class="alert alert-warning" role="alert">Something went wrong!</div>';
+
+                        jQuery(modalBody).html(response);
+                        jQuery(modalElement).modal('show');
+
+                        setTimeout(function () {
+                            jQuery(modalElement).modal('hide');
+                        }, 7000);
+                    }
+                });
             }
-        });
+        }, 1200);
 
     }
 
